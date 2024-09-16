265b1846e8ab04474ad870ce11a9752bcb7286da
tomas <tomasz.pielecki86@gmail.com>
2024-09-16 11:13:41 +0200

    Historia Firmy Footer

--- Components/Footer.php
+++ Components/Footer.php
@@ -1,3 +1,33 @@
 <footer>
-    <p>tomasz.pielecki86@gmail.com © 2024</p>
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h4>Historia firmy</h4>
+                <p>
+                    Nasza firma rozpoczęła swoją działalność ponad 30 lat temu, w skromnych warunkach. 
+                    W 1994 roku, zaczynając od niewielkiego garażu i sprzedaży z samochodu, 
+                    postanowiliśmy rozwinąć naszą działalność. Po kilku latach ciężkiej pracy i determinacji 
+                    udało nam się zakupić magazyn, co pozwoliło na znaczne zwiększenie naszych możliwości operacyjnych.
+                </p>
+                <p>
+                    Początkowo korzystaliśmy z ciężarówek marki <strong>Star</strong>, <strong>Zil</strong> oraz <strong>Robur</strong>, 
+                    które były niezawodne w naszych codziennych operacjach. W miarę jak firma rosła, inwestowaliśmy w nowoczesny sprzęt – 
+                    najpierw zakupiliśmy pojazdy <strong>Iveco</strong>, a w końcu przeszliśmy na najbardziej renomowane marki – 
+                    <strong>DAF</strong> i <strong>Scania</strong>.
+                </p>
+                <p>
+                    Nasza flota stała się symbolem jakości i profesjonalizmu, a nasze usługi zyskały uznanie w branży.
+                </p>
+                <p>
+                    Dziś, jako liderzy w naszej dziedzinie, kontynuujemy rozwój, dbając o najwyższe standardy obsługi 
+                    i innowacje technologiczne. Nasza historia to opowieść o pasji, wzroście i nieustannym dążeniu do doskonałości.
+                </p>
+                <p><strong>Developer: </strong></p>
+                <h4>tomasz.pielecki86@gmail.com © 2024</h4>
+            </div>
+        </div>
+    </div>
 </footer>
+
+
